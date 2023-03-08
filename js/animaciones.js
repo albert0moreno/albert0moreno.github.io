a769a3ac2fd4382bca7746c1f967d7bd37895494
albert0moreno <76948093+albert0moreno@users.noreply.github.com>
2023-03-08 01:14:05 +0100

    Delete js directory

--- js/animaciones.js
+++
@@ -1,7 +0,0 @@
-
-// BOTÓN TELEGRAM
-window.onload = function botonTelegram() {
-    setTimeout(function botonTelegram () {
-        document.getElementById("TELEGRAM").style.display = "block";
-    }, 5000);
-};
