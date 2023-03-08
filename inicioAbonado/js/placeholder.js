a60dc9148f372423af980f2faba69cd3848fce4a
albert0moreno <76948093+albert0moreno@users.noreply.github.com>
2023-03-08 01:13:56 +0100

    Delete inicioAbonado directory

--- inicioAbonado/js/placeholder.js
+++
@@ -1,6 +0,0 @@
-function plegarTexto(){
-    let x = document.getElementById("inputUser");
-    if(x.value<=0){
-        document.getElementById("inputUser").style.placeholder.fontSize = "8px";
-    }
-}
